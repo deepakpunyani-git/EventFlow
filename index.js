ba5710b8a0b38fc863501be7b41b3a7577adcf1f
Deepak <deepakpunyani@ymail.com>
2024-04-28 22:57:40 +0530

    email test

--- index.js
+++ index.js
@@ -9,14 +9,45 @@ const connectDB = require('./db');
 const PORT = process.env.PORT || 3000;
 const app = express();
 const routes = require('./routes/index');
+const nodemailer = require("nodemailer");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-app.get("/", (req, res) => {
-  res.send('Demo APIs - EventFlow. <a href="/api-docs/">Test APIs here</a>.');
-})
+app.get("/", async (req, res) => {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.EMAIL_PORT, // Should be process.env.EMAIL_PORT
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+      tls: {
+        rejectUnauthorized: true,
+      },
+    });
+    const  to = 'deepakpunyani@ymail.com';
+    const subject = "test";
+    const content = "asdasdadadasdsadsad";
+    const info = await transporter.sendMail({
+      to: to,
+      subject: subject,
+      text: content,
+      html: content,
+    });
+
+    console.log(info.envelope);
+    console.log("Message sent: %s", info.messageId);
+
+    res.send('Demo APIs - EventFlow. <a href="/api-docs/">Test APIs here</a>.');
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
 // Swagger setup
 const options = {
